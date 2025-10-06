01561892afa743318bc05a6f04399b8e631853c9
Mateo Pretelli <86113904+mateopretelli@users.noreply.github.com>
2025-10-05 22:29:02 -0300

    StuderntCourse admin view initialization

---
+++ WindowsForms/FormStudentCourse/StudentCourseApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms.FormStudentCourse
+{
+    internal class StudentCourseApiClient
+    {
+    }
+}
